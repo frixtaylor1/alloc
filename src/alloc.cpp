88de6e5ac81ea5e17b6306cafdd2f0709d19d6a9
kevin daniel taylor <taylorfrix123@gmail.com>
2025-06-06 21:49:02 -0300

    |

--- src/alloc.cpp
+++ src/alloc.cpp
@@ -2,100 +2,109 @@
 #include <assert.h>
 #include <string.h>
 
-$hidden
-byte *lastAddress = ($1 memspace + (1024 * 1024 * 1024));
+$hidden byte *lastAddress = ($1 memspace + (1024 * 1024 * 1024));
 
-struct alignas(4)
-Header {
-  Header(): words(0), alloced(false) {
-}
+struct alignas(4) Header {
+  Header() : words(0), alloced(false) {}
   word words : 30;
   bool alloced : 1;
   [[maybe_unused]] bool reserved : 1;
 };
 
+$hidden void _init_header(Header *header, word words, bool alloced, bool reserved = false) {
+  header->words = words;
+  header->alloced = alloced;
+  header->reserved = reserved;
+}
+
 #define $h (Header *)
 
-
-$hidden
-word _calculate_words_given_bytes(int32 bytes) {
+$hidden word _calculate_words_given_bytes(int32 bytes) {
   return !(bytes % 4) ? bytes / 4 : bytes / 4 + 1;
 }
 
-$hidden
-Header *_get_header_given_ptr(void *ptr) {
-  return $h ptr - 1;
-}
+$hidden Header *_get_header_given_ptr(void *ptr) { return $h ptr - 1; }
 
-$hidden
-int32 _get_bytes_alloc_region_given_header(Header *header) {
+$hidden int32 _get_bytes_alloc_region_given_header(Header *header) {
   return header->words * 4;
 }
 
-$hidden
-Header *_next_header(Header *header) {
-  return $h ($1 (header + 1) + header->words * 4);
+$hidden Header *_next_header(Header *header) {
+  return $h($1(header + 1) + header->words * 4);
 }
 
-$hidden
-Header *_find_block(Header *current, word wordsToAlloc) {
+$hidden Header *_find_block(Header *current, word wordsToAlloc) {
   if ($1 current >= lastAddress) {
     return nullptr;
   }
-  if (!current->alloced) return current;
+  if (!current->alloced)
+    return current;
 
   return _find_block(_next_header(current), wordsToAlloc);
 }
 
-$visible
-void *alloc(int32 bytes) {
-  void   *mem         = memspace;
-  Header *header      = $h mem;
-  word   wordsToAlloc = _calculate_words_given_bytes(bytes);
+$hidden void _coalesce(Header *header) {
+  Header *next = _next_header(header);
+  if (next && !next->alloced) {
+    header->words += 1 + next->words;
+  }
+}
 
-  if (!bytes) return nullptr;
+$visible void *alloc(int32 bytes) {
+  void *mem = memspace;
+  Header *header = $h mem;
+  word wordsToAlloc = _calculate_words_given_bytes(bytes);
+
+  if (!bytes)
+    return nullptr;
 
   if (!header->alloced) {
     if (header->words == 0) {
-      header->words = wordsToAlloc;
-      header->alloced = true;
-      header->reserved = false;
+      _init_header(header, wordsToAlloc, true);
       return $h mem + 1;
     }
 
     if (header->words >= wordsToAlloc) {
-      Header *next   = _next_header(header);
-      next->words    = wordsToAlloc;
-      next->alloced  = false;
-      next->reserved = false;
+      word difference = header->words - wordsToAlloc;
+      _init_header(header, wordsToAlloc, true);
 
-      header->words = wordsToAlloc;
-      header->alloced = true;
-      return $v (header + 1);
+      Header *next = _next_header(header);
+      _init_header(next, difference, false);
+
+      return $v(header + 1);
     }
   }
 
   Header *found = _find_block(header, wordsToAlloc);
+  if (found->words) {
+    word difference = found->words - wordsToAlloc;
+    found->words = wordsToAlloc;
+
+    Header *next = _next_header(found);
+    _init_header(next, difference, false);
+  }
+
   if (found) {
     found->alloced = true;
-    return $v (found + 1);
+    return $v(found + 1);
   }
 
   assert(false && "Error no mem!");
 }
 
-$visible
-void dalloc(void *ptr) {
+$visible void dalloc(void *ptr) {
   Header *header = _get_header_given_ptr(ptr);
 
-  if (!header->alloced) assert(header->alloced && "Double free! %d");
+  if (!header->alloced)
+    assert(header->alloced && "Double free! %d");
   header->alloced = 0;
 
   ::memset(ptr, 0, _get_bytes_alloc_region_given_header(header));
+
+  _coalesce(header);
 }
 
-$visible
-void *ralloc(void *ptr, int32 bytes) {
+$visible void *ralloc(void *ptr, int32 bytes) {
   Header *header = _get_header_given_ptr(ptr);
 
   word wordsNeeded = _calculate_words_given_bytes(bytes);
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include "../include/alloc.hpp"
+#include <stdio.h>
 
 int main(int argc, const char *argv[]) {
   (void) argc;
   (void) argv;
+
+
+  void *a = alloc(400);
+  dalloc(a);
+
+  void *b = alloc(350);
+
+  void *c = alloc(40);
+
+  dalloc(b);
+  printf("%p\n", a);
+  printf("%p\n", b);
+  printf("%p\n", c);
+
   return 0;
 }
